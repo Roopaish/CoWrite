7d1b6745fdb615f42e1c6c4db9c2e16297ba859e
Rupesh Budhathoki <54392112+Roopaish@users.noreply.github.com>
2023-02-21 11:09:39 +0545

    Logger setup

---
+++ lib/pages/cowrite.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CoWrite extends StatefulWidget {
+  const CoWrite({super.key});
+
+  @override
+  State<CoWrite> createState() => _CoWriteState();
+}
+
+class _CoWriteState extends State<CoWrite> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
